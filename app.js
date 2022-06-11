23399090b06eca849bb2ae2d2d7dd7d92dd26714
dev.masud0X0 <76620767+Masud-Ndatsu@users.noreply.github.com>
2022-06-11 09:19:46 +0100

    api integration initiated

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const form = document.querySelector(".form-group");
+const output = document.querySelector(".output");
+const input = document.querySelector("#url");
+
+const token = "010b7dc6e9da2c1a7251d562961e399f23f2334e";
+const url = "https://api-ssl.bitly.com/v4/shorten";
 
 form.addEventListener("submit", postFormData);
 
-function postFormData(e) {
+async function postFormData(e) {
   e.preventDefault();
+  const url = input.value;
+  const postObj = {
+    domain: "bit.ly",
+    long_url: url,
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer bc48f2aa48032f2100c4becfad1067465069fba1`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postObj),
+    });
+
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log({ error: err.message });
+  }
 }
